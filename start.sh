92c66cd114e1148a365aeace95da9850f8447d61
Miroslav Spousta <qiq@ucw.cz>
2018-04-04 11:27:02 +0200

    Make ssh more resilient to connectivity outage

--- start.sh
+++ start.sh
@@ -2,4 +2,5 @@
 
 ssh-keyscan $SSH_HOST >> ~/.ssh/known_hosts
 
-ssh -i $ID_FILE -nNT -o GatewayPorts=true -L $LOCAL_PORT:$REMOTE_HOST:$REMOTE_PORT $SSH_USER@$SSH_HOST
+while true; do ssh -i $ID_FILE -nNT -o GatewayPorts=true -o ServerAliveInterval=60 -o ServerAliveCountMax=10 -L $LOCAL_PORT:$REMOTE_HOST:$REMOTE_PORT $SSH_USER@$SSH_HOST; sleep 10; done
+
